888020d8d8384ee30fd9c36c44b9017213b1121a
Matheus <mamisumi@gmail.com>
2023-05-30 01:09:20 -0300

    clustering with pm2 deprecate manual clustering

--- main/index.js
+++ main/index.js
@@ -1,29 +1,18 @@
 process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require('cluster');
 
-// Is the file being execute in master mode?
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
+const PORT = 3000;
 
-if (cluster.isMaster) {
-    // Cause index.js to be executed *again* but 
-    // in child mode
-    cluster.fork();
-    cluster.fork();
-} else {
-    // Child, acting like a server, and nothing else.
-    const express = require('express');
-    const crypto = require('crypto');
-    const app = express();
-    const PORT = 3000;
-
-    app.get('/', (req, res) => {
-        crypto.pbkdf2('a', 'b', 10000, 512, 'sha512', () => {
-            res.send('Hi there');
-        });
+app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 10000, 512, 'sha512', () => {
+        res.send('Hi there');
     });
+});
 
-    app.get('/fast', (req, res) => {
-        res.send('This was fast');
-    })
+app.get('/fast', (req, res) => {
+    res.send('This was fast');
+})
 
-    app.listen(PORT);
-}
+app.listen(PORT);
